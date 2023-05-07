c46afcaa3e60d95c904c0e55ef7740210cb46d40
Shriyash-Kadam <126370082+Shriyash-Kadam@users.noreply.github.com>
2023-05-07 11:36:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function bedroom() {
+      window.location = "bedroom.html";
+  }
+
+  function tv() {
+    window.location = "tv.html";
+}
+  
+function garage() {
+    window.location = "garage.html";
+}
+  
+
+function desk() {
+    window.location = "desk.html";
+}
+  
+
+function fruit() {
+    window.location = "f.html";
+}
+  
